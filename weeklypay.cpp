119ea320cb12068bf4441beb0044db9aff2e5330
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-04-02 15:11:17 -0400

    Add files via upload

---
+++ weeklypay.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     double hours,rate,pay;
+
+     cout<<"How many hours did you work? ";
+     cin >> hours;
+
+     cout << "What is the pay rate? ";
+     cin >> rate; 
+
+     pay = rate * hours;
+
+     cout << "Your total pay is " << pay << " dollars";
+
+     return 0;
+
+
+
+
+
+}
