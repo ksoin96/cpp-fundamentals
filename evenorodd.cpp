1780bf60b56c264ead288e21cd453fe5b10a1d60
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-04-15 17:58:58 -0400

    Add files via upload

---
+++ evenorodd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+   int number;
+   std::cout << "What is your number? ";
+   std::cin >> number;
+
+   if (number%2==0){
+      std::cout << "Even";
+
+   }
+   else{
+      std::cout << "Odd";
+   }
+
+   return 0;
+
+}
